bae5ee39141c0afe620223342d1fdbfe39736930
mrbandler <baudler.michael@gmail.com>
2019-05-31 16:07:25 +0200

    Figured the compilation process out and added command line options

---
+++ ks-compiler/Options.cs
@@ -0,0 +1,28 @@
+﻿using CommandLine;
+
+namespace KSCompiler
+{ 
+    /// <summary>
+    /// Commandline interface options.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Input filename or directory.
+        /// </summary>
+        [Option('i', "input", Required = true, HelpText = "Kerboscript input filename or path to a directory containing the Kerboscript source code.")]
+        public string Input { get; set; }
+
+        /// <summary>
+        /// Output filename or directory.
+        /// </summary>
+        [Option('o', "output", Required = false, HelpText = "Kerboscript output filename or path to a directory where the compiled Kerboscript should be written to; defaults to './objs'")]
+        public string Output { get; set; } = "obj";
+
+        /// <summary>
+        /// Indication wheter the compiler should watch the input file or directory for changes and recompile when changes are found.
+        /// </summary>
+        [Option('w', "watch", Required = false, HelpText = "Wheter the compiler should watch the input file or directory for changes and recompile when changes are found.")]
+        public bool Watch { get; set; }
+    }
+}
---
+++ ks-compiler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CommandLine;
+
+namespace KSCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o => Run(o));
+        }
+
+        static void Run(Options options)
+        {
+            Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
+            Console.WriteLine("Input: " + options.Input);
+            Console.WriteLine("Output: " + options.Output);
+        }
+    }
+}
